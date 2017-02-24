3816f959b0021dbaa7a9bd2aa14edab9b833f399
drake-jin <dydwls121200@gmail.com>
2017-02-24 16:52:34 +0900

    Skectch a function that Read Email and Get Torrent
    Current Branch : dev-IMAPRead
    Updated Desc
        Updated : .env
            Change Pyenv environment or Project Environment Values are modified
        Updated : .gitignore
            Add log , __pycache__ folder
        Updated : config.json
            Define config file
        Updated : init.sh
            Modified lisst of required librarys
        Created : log/develop.log
            This program write log file on log/develop.log   log/tester.log...
        Created : src/imap/imap.py
            Skectch what i want to do.
        Created : src/imap/logger.py
            Define Logger class.
        Created : src/imap/transmission_rpc.py
            This is harlov's source code. It will be changed.
        Created : src/modules/__init__.py
            touch __init__.py.
        Created : src/modules/logger.py
            This modules directory will commom custom modules
    
    1. [x] Connect with Email through IMAP Protocol
    2. [x] Connect with Transmission Web Controller throu http Protocol
    3. [x] Read folder of Email List And Get torrent files.
    4. [x] The torrent files will be transported to transmission web controller.
    5. [ ] If the 4th work were Okey, This program send through READ flag to
    Connected Email Account.
    6. [ ] At the same time, Send an mail to connected account what transmission
    successfully regist your torrent files with detail information.

--- init.sh
+++ init.sh
@@ -2,15 +2,16 @@
 
 # Recommand autoenv 
 # If you aren't autoenv user 
-# execute this command line
-# when $ source PROJECT_HOME/.env
+# execute this command line that '$ source PROJECT_HOME/.env'
 
 source `pwd` .env
 
 pyenv install 3.4.3
 
-pyenv virtualenv 3.4.3 $PROJECT_NAME_WEB
-pyenv virtualenv 3.4.3 $PROJECT_NAME_IMAP
+pyenv virtualenv 3.4.3 $PROJECT_NAME
 
-ln -s `pyenv root`/versions/$PROJECT_NAME_WEB $PROJECT_HOME/src/web/$PROJECT_NAME_WEB
-ln -s `pyenv root`/versions/$PROJECT_NAME_IMAP $PROJECT_HOME/src/imap/$PROJECT_NAME_IMAP
+ln -s `pyenv root`/versions/$PROJECT_NAME $PROJECT_HOME/$PROJECT_NAME
+
+pyenv shell $PROJECT_NAME
+pip install --upgrade pip
+
