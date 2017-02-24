b8f090cb0ccb5d70b0ce8a75fc0d46903ef50662
drake-jin <dydwls121200@gmail.com>
2017-02-24 16:52:34 +0900

    Skectch a function that Read Email and Get Torrent
    Current Branch : dev-IMAPRead
    Updated Desc
        Updated : .env
            Change Pyenv environment or Project Environment Values are modified
        Updated : .gitignore
            Add log , __pycache__ folder
        Updated : config.json
            Define config file
        Updated : init.sh
            Modified lisst of required librarys
        Created : log/develop.log
            This program write log file on log/develop.log   log/tester.log...
        Created : src/imap/imap.py
            Skectch what i want to do.
        Created : src/imap/logger.py
            Define Logger class.
        Created : src/imap/transmission_rpc.py
            This is harlov's source code. It will be changed.
        Created : src/modules/__init__.py
            touch __init__.py.
        Created : src/modules/logger.py
            This modules directory will commom custom modules
    
    1. [x] Connect with Email through IMAP Protocol
    2. [x] Connect with Transmission Web Controller throu http Protocol
    3. [x] Read folder of Email List And Get torrent files.
    4. [x] The torrent files will be transported to transmission web controller.
    5. [ ] If the 4th work were Okey, This program send through READ flag to
    Connected Email Account.
    6. [ ] At the same time, Send an mail to connected account what transmission
    successfully regist your torrent files with detail information.

---
+++ src/imap/imap.py
@@ -0,0 +1,79 @@
+import imaplib
+import json
+import os
+import time
+from email import parser
+import email
+from logger import DJLogger
+from transmission_rpc import TransmissionRpc
+
+logger = DJLogger.getLogger()
+
+logger.debug("read")
+
+with open(os.environ['PROJECT_HOME']+'/config.json') as configJson:
+    logger.debug(configJson)
+    configData = json.load(configJson)
+    logger.debug(configData)
+
+
+emailConnection = imaplib.IMAP4_SSL(
+    configData['email_host'],
+    configData['email_port']
+)
+emailConnection.login(
+    configData['email_user'],
+    configData['email_password']
+)
+
+transmissionConnection = TransmissionRpc(configData)
+
+def proc_email(message, mail_id):
+    msg = email.message_from_bytes(message)
+
+    for part in msg.walk():
+        logger.debug(part.get_content_type())
+        if part.get_content_type() == 'application/x-bittorrent':
+            torrent_data_64 = part.get_payload()
+            logger.debug(torrent_data_64)
+            transmissionConnection.request(
+                "torrent-add",
+                {"metainfo": torrent_data_64}
+            )
+
+
+def run():
+    logger.debug("run")
+    # transmissionRpc(config)
+
+    while True:
+        time.sleep(configData['email_check_interval'])
+        logger.debug("loop start----------------------- ")
+        logger.debug("-- Check Email ")
+        emailConnection.select(configData['email_input_folder'])
+        logger.debug(emailConnection.search(None, 'ALL'))
+
+        # result, data = emailConnection.uid('SEARCH', None, '(UNSEEN)')
+        result, data = emailConnection.uid('SEARCH', None, '(UNSEEN)')
+
+        logger.debug(result)
+        logger.debug(data)
+        logger.debug(data[0])
+        uid_list = data[0]
+        uids = uid_list.split()
+
+        for uid in uids:
+            try:
+                result, data = emailConnection.uid('fetch', uid, '(RFC822)')
+                proc_email(data[0][1], uid.decode('UTF-8'))
+            except Exception as e:
+                logger.debug(e)
+
+        logger.debug("loop end----------------------- ")
+
+
+run()
+
+
+
+
