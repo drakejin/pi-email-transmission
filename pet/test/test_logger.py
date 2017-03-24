122410c0a20eceee5882db9cabcd6b5627976877
drake-jin <dydwls121200@gmail.com>
2017-03-24 15:16:48 +0900

    travis has occured about test

--- pet/test/test_logger.py
+++ pet/test/test_logger.py
@@ -10,7 +10,7 @@ from pet.__main__ import main
 
 class TestPET(TestCase):
     def test_is_logger(self):
-        logger = Logger.getLogger()
+        logger = Logger().getLogger()
         logger.debug('So curious!')
         self.assertTrue(1234, 1234)
 
